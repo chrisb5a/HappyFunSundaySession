626c4747c25acfde73a521dab7fcf1e7316972c4
CHRISTIAN BEYNIS <christianbeynis@CHRISTIANs-MBP.lan>
2022-10-16 17:52:47 -0400

    new function

--- src/ios/HappyFunSundaySession.swift
+++ src/ios/HappyFunSundaySession.swift
@@ -39,4 +39,52 @@
       callbackId: command.callbackId
     )
   }
+  
+  @objc(adds:) func adds(_ command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+        let param1 = (command.arguments[0] as? NSObject)?.value(forKey: "param1") as? Int
+        let param2 = (command.arguments[1] as? NSObject)?.value(forKey: "param2") as? Int
+
+        if let p1 = param1 , let p2 = param2 {
+            if p1 >= 0 && p2 >= 0{
+                let total = String(p1 + p2)
+
+                if total.characters.count > 0 {
+                    let toastController: UIAlertController =
+                     UIAlertController(
+                        title: "",
+                        message: total,
+                        preferredStyle: .alert
+                    )
+
+                    self.viewController?.present(
+                        toastController,
+                        animated: true,
+                         completion: nil
+                        )
+
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        toastController.dismiss(
+                            animated: true,
+                            completion: nil
+                        )
+                    }
+
+
+
+
+                pluginResult = CDVPluginResult(status: CDVCommandStatus_OK,
+                                               messageAs: total)
+            } else {
+                pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR,
+                                               messageAs: "Something wrong")
+            }
+        }
+        self.commandDelegate!.send(pluginResult,
+                                   callbackId: command.callbackId)
+    }
+
+
+
+
 }
--- www/HappyFunSundaySession.js
+++ www/HappyFunSundaySession.js
@@ -2,6 +2,11 @@ var exec = require('cordova/exec');
 exports.echo = function(arg0, success, error) {
   exec(success, error, 'HappyFunSundaySession', 'echo', [arg0]);
 };
+
+module.exports.adds = function (arg0, success, error) {
+    exec(success, error, 'HappyFunSundaySession', 'adds', [arg0, arg1]);
+};
+
 exports.echojs = function(arg0, success, error) {
   if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
     success(arg0);
